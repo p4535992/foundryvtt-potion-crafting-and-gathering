c6e27e7cf49c6efea1ba42ddb587bb6604010179
p4535992 <tentimarco0@gmail.com>
2023-10-06 20:28:21 +0200

    version 0.7.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,5 @@
+### 0.7.1
+
+- Transfer all the code the single module.js file
+
 ### 0.5.0-1
--- src/module.js
+++ src/module.js
@@ -7,8 +7,9 @@ const CONSTANTS = {
   PATH: `modules/potion-crafting-and-gathering/`,
   PACK_UUID_ROLLTABLES: "potion-crafting-and-gathering.potion-crafting-and-gathering-tables",
   PACK_UUID_JOURNALS: "potion-crafting-and-gathering.potion-crafting-and-gathering-journal",
-  MODULE_FOLDER: `modules/potion-crafting-and-gathering/recipes`,
+  MODULE_FOLDER: `modules/potion-crafting-and-gathering/assets/recipes`,
   PACKS: ["potion-crafting-and-gathering-tables", "potion-crafting-and-gathering-journal"],
+  GATHERER_MODULE_ID: "gatherer",
 };
 
 /* ------------------------------------ */
@@ -44,7 +45,8 @@ export const registerSettings = function () {
 /* ------------------------------------ */
 
 const API = {
-  // You can put here some code and call on a macro with game.modules.get("potion-crafting-and-gathering").api
+  // You can put here some code and call on a macro
+  // with game.modules.get("potion-crafting-and-gathering").api
 };
 
 /* ------------------------------------ */
@@ -78,7 +80,7 @@ Hooks.once("ready", async () => {
     title: game.modules.get(CONSTANTS.MODULE_ID).title, // "Potion Crafting & Gathering",
     content: "Do you want to import all tables, journals and recipes?",
     yes: () => {
-      if (!game.modules.get("gatherer")?.active && game.user?.isGM) {
+      if (!game.modules.get(CONSTANTS.GATHERER_MODULE_ID)?.active && game.user?.isGM) {
         ui.notifications.warn(`You need to install and activate the module premium 'gatherer' for this`);
         ui.notifications.warn(`You can still use the compendiums anyway as normal`);
       } else {
