5e4dcd382c8a5f58b5006c8f2fb093877e78077a
p4535992 <tentimarco0@gmail.com>
2024-02-19 21:33:05 +0100

    update jsdoc

--- src/lib.js
+++ src/lib.js
@@ -26,30 +26,137 @@
  */
 
 // =======================
-// Beaver Crafting models
+// Beaver Crafting models (version 3.5.2)
 // =======================
 
 /**
- * @typedef {Object} IngredientBeaverCrafting
- * @property {string} id -
- * @property {string} uuid -
- * @property {string} type -
- * @property {string} name -
- * @property {string} image -
- * @property {number} quantity -
- * @property {Object} getEntity -
- * @property {Object} isSame -
+ * @typedef {Object} ResultDataBeaverCrafting
+ * @property {Object.<string, string>}  _actorUpdate
+ * @property {boolean} _hasException
+ * @property {{required: ComponentResultsDataBeaverCrafting,consumed: ComponentResultsDataBeaverCrafting,produced: ComponentResultsDataBeaverCrafting}} _components
+ * @property {{name: string,dc: number,total: number}|undefined} _skill
+ * @property {{hits: number,fails: number,maxFails:number,maxHits: number}|undefined} _tests
+ * @property {CurrencyResultDataBeaverCrafting|undefined} _currencyResult
+ * @property {Object.<string, ComponentChatDataBeaverCrafting>} _chatAddition
+ * @property {RecipeDataBeaverCrafting} _recipe
  */
 
 /**
- * @typedef {Object} RecipeBeaverCrafting
- * @property {Object.<string, IngredientBeaverCrafting>} ingredients -
- * @property {Object.<string, IngredientBeaverCrafting>} results -
- * @property {value:number, name: string} currency -
- * @property {Object} attendants -
- * @property {fails:number, consume: boolean, ands: {[k: string|number]: {hits:string|number, ors: {[k: string|number]:{ check:string, type:string, uuid:string}}}}} tests -
- * @property {string} macro -
+ * @typedef {Object} CraftingDataBeaverCrafting
+ * @property {string|undefined} uuid
+ * @property {string} name
+ * @property {string} img
+ * @property {number} startAt
+ * @property {number} lastAt
+ * @property {number} endAt
+ * @property {ResultDataBeaverCrafting} result
+ * @property {RecipeDataBeaverCrafting} recipe
+ * @property {Boolean|undefined} isFinished
+ * @property {Object[]} restore
+}
+
+/**
+ * @typedef {Object} ComponentResultsDataBeaverCrafting
+ * @property {ComponentResultDataBeaverCrafting[]} _data
  */
+
+/**
+ * @typedef {Object} ComponentResultDataBeaverCrafting
+ * @property {ComponentDataBeaverCrafting} component
+ * @property {Boolean} isProcessed
+ * @property {number} originalQuantity
+ * @property {UserInteractionBeaverCrafting} userInteraction
+ */
+
+/**
+ * @typedef {"always" | "never" | "onSuccess"} UserInteractionBeaverCrafting
+ */
+/**
+ * @typedef {"consumed" | "required" | "produced"}  ComponentTypeBeaverCrafting
+ */
+
+/**
+ * @typedef {"required" | "input" | "output"} DataTypeBeaverCrafting
+ */
+
+/**
+ * @typedef {"skill" | "tool" | "ability" | "hit"} TestTypeBeaverCrafting
+ */
+
+/**
+ * @typedef {"success"|"error"|"locked"|"undefined"|"unknown"} ComponentStatusBeaverCrafting
+ */
+
+/**
+ * @typedef {Object} ComponentChatDataBeaverCrafting
+ * @property {ComponentDataBeaverCrafting} component
+ * @property {ComponentTypeBeaverCrafting} type
+ * @property {ComponentStatusBeaverCrafting} status
+ * @property {Boolean} isProcessed
+ */
+
+/**
+ * @typedef {Object} TestsBeaverCrafting
+ * @property {number} fails
+ * @property {Boolean} consume
+ * @property {Object.<number, TestOrBeaverCrafting>} ands
+ */
+
+/**
+ * @typedef {Object} TestAndBeaverCrafting
+ * @property {number} hits
+ * @property {Object.<number, TestOrBeaverCrafting>} ors
+ */
+
+/**
+ * @typedef {Object} TestOrBeaverCrafting
+ * @property {number} check
+ * @property {TestTypeBeaverCrafting} type
+ * @property {string} uuid
+ */
+
+/**
+ * @typedef {Object} SkillBeaverCrafting
+ * @property {string} name
+ * @property {number} dc
+ * @property {Boolean} consume
+ */
+
+/**
+ * @typedef {Object} CurrencyBeaverCrafting
+ * @property {string} name
+ * @property {number} value
+ */
+
+/**
+ * @typedef {Object} ComponentDataBeaverCrafting
+ * @property {string} id
+ * @property {string} uuid
+ * @property {string} type
+ * @property {string} name
+ * @property {string} img
+ * @property {number} quantity
+ * @property {string|undefined} itemType if it is of type item there is an itemType
+ * @property {string|undefined} jsonData to store a component completely
+ * @property {Object.<string, Object>|undefined} flags module specific flags (the key is the module id )
+ */
+
+/**
+ * @typedef {Object} RecipeDataBeaverCrafting
+ * @property {Object.<string, Object.<string, ComponentDataBeaverCraftingBeaverCrafting>>} input
+ * @property {Object.<string, Object.<string, ComponentDataBeaverCraftingBeaverCrafting>>} output
+ * @property {Object.<string, Object.<string, ComponentDataBeaverCraftingBeaverCrafting>>} required
+ * @property {Object.<string, ComponentDataBeaverCrafting>|undefined} ingredients
+ * @property {Object.<string, ComponentDataBeaverCrafting>|undefined} results
+ * @property {Object.<string, ComponentDataBeaverCrafting>|undefined} attendants
+ * @property {TestsBeaverCrafting|undefined} tests
+ * @property {SkillBeaverCrafting|undefined} skill
+ * @property {CurrencyBeaverCrafting|undefined} currency
+ * @property {string|undefined} tool
+ * @property {string|undefined} macro
+ * @property {string|undefined} folder
+ * @property {string|undefined} instruction
+}
 
 // =======================
 // Macros for conversion models
